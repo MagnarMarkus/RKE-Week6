1b90111f431a5423a66610bea697de753c25f350
MagnarMarkus <magnar-markus.lill@tktk.ee>
2024-05-21 17:10:23 +0300

    wee 6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] snacks = { "sushi", "poke", "burx", "pasta", "kfc", "chips" };
+
+Random rnd = new Random();
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
